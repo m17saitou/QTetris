35d7d17cc2da3dd66bd66ed7c43148f7f8ca95f6
m17saitou <m17saitou@gmail.com>
2019-11-01 03:31:19 +0900

    Add:とりあえず画面の中で文字を出力できる状態にした

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,11 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     testLabel = new QLabel("Hello");
+    QHBoxLayout *main = new QHBoxLayout();
+    main->addWidget(testLabel);
+    QWidget *widget = new QWidget();
+    widget->setLayout(main);
+    setCentralWidget(widget);
 }
 
 MainWindow::~MainWindow()
